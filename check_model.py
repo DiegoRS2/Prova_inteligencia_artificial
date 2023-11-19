d77bde5bfc451094fcc00fd9ebcf3c0c92a2f4d4
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2023-11-19 15:25:36 -0300

    ajustes

--- check_model.py
+++ check_model.py
@@ -1,73 +1,118 @@
 from itertools import permutations
 
-def check_conditions(houses):
-    for i in range(5):
-        if houses[i]['nationality'] == 'norueguês' and i != 0:
-            if houses[i - 1]['color'] != 'azul' and houses[i + 1]['color'] != 'azul':
-                return False
 
-        if houses[i]['color'] == 'verde':
-            if i == 0 or i == 4:
-                return False
-            if houses[i - 1]['color'] != 'branca' or houses[i + 1]['color'] != 'branca':
-                return False
+class Symbol():
+    def __init__(self, name):
+        self.name = name
 
-        if houses[i]['nationality'] == 'britânico' and houses[i]['color'] != 'vermelha':
-            return False
+    def __repr__(self):
+        return self.name
 
-        if houses[i]['nationality'] == 'suíço' and houses[i]['profession'] != 'mecânico':
-            return False
+    def __eq__(self, other):
+        return isinstance(other, Symbol) and self.name == other.name
 
-        if houses[i]['nationality'] == 'dinamarquês' and houses[i]['drink'] != 'chá':
-            return False
+    def __hash__(self):
+        return hash(self.name)
 
-        if houses[i]['color'] == 'amarela' and houses[i]['cigar'] != 'Dunhill':
-            return False
 
-        if houses[i]['profession'] == 'professor' and houses[i]['cigar'] != 'Pall Mall':
-            return False
+class Symbol:
+    def __init__(self, name):
+        self.name = name
 
-        if houses[i]['cigar'] == 'Blends':
-            if (i > 0 and houses[i - 1]['profession'] != 'empresário') and (i < 4 and houses[i + 1]['profession'] != 'empresário'):
-                return False
+    def __repr__(self):
+        return self.name
 
-        if houses[i]['cigar'] == 'Blends':
-            if (i > 0 and houses[i - 1]['drink'] != 'água') and (i < 4 and houses[i + 1]['drink'] != 'água'):
-                return False
+    def evaluate(self, model):
+        return model[self]
 
-        if houses[i]['drink'] == 'leite' and i != 2:
-            return False
+    def __eq__(self, other):
+        return isinstance(other, Symbol) and self.name == other.name
 
-        if houses[i]['cigar'] == 'Bluemasters' and houses[i]['drink'] != 'cerveja':
-            return False
+    def __hash__(self):
+        return hash(self.name)
 
-        if houses[i]['nationality'] == 'alemão' and houses[i]['cigar'] != 'Prince':
-            return False
 
-    return True
+class And:
+    def __init__(self, *conjuncts):
+        self.conjuncts = list(conjuncts)
 
-def solve_puzzle():
-    colors = ['vermelha', 'verde', 'branca', 'amarela', 'azul']
-    nationalities = ['britânico', 'suíço', 'dinamarquês', 'norueguês', 'alemão']
-    drinks = ['chá', 'café', 'leite', 'cerveja', 'água']
-    cigars = ['Pall Mall', 'Dunhill', 'Blends', 'Bluemasters', 'Prince']
-    professions = ['professor', 'empresário', 'mecânico', 'programador', 'terrorista']
+    def __repr__(self):
+        conjunctions = ", ".join([str(conjunct) for conjunct in self.conjuncts])
+        return f"And({conjunctions})"
 
-    all_permutations = permutations([
-        {'color': color, 'nationality': nationality, 'drink': drink, 'cigar': cigar, 'profession': profession}
-        for color in colors
-        for nationality in nationalities
-        for drink in drinks
-        for cigar in cigars
-        for profession in professions
-    ])
+    def evaluate(self, model):
+        return all(conjunct.evaluate(model) if isinstance(conjunct, Symbol) else conjunct for conjunct in self.conjuncts)
 
-    for houses in all_permutations:
-        if check_conditions(houses):
-            for i in range(5):
-                if houses[i]['profession'] == 'terrorista':
-                    return houses[i]['nationality']
 
-# Resolvendo o quebra-cabeça e encontrando o terrorista
-terrorist = solve_puzzle()
-print(f"O terrorista é o {terrorist}.")
+class Not:
+    def __init__(self, operand):
+        self.operand = operand
+
+    def __repr__(self):
+        return f"Not({self.operand})"
+
+    def evaluate(self, model):
+        return not self.operand.evaluate(model) if isinstance(self.operand, Symbol) else self.operand
+
+
+# Restante do código...
+
+
+def generate_symbols(prefix, count):
+    return [Symbol(f'{prefix}{i}') for i in range(1, count + 1)]
+
+
+# Definição dos símbolos para cada propriedade
+nacionalidade = generate_symbols('n', 5)
+cor_casa = generate_symbols('c', 5)
+bebida = generate_symbols('b', 5)
+cigarro = generate_symbols('s', 5)
+profissao = generate_symbols('p', 5)
+terrorista = generate_symbols('t', 5)
+
+# Definição das sentenças lógicas representando as informações adicionais
+knowledge_base = And(
+    nacionalidade[0], cor_casa[0],  # 1 - O britânico mora na casa vermelha
+    nacionalidade[1], profissao[1],  # 2 - O suíço é mecânico
+    nacionalidade[2], bebida[2],  # 3 - O dinamarquês bebe chá
+    cor_casa[1], cor_casa[0],  # 4 - A casa verde fica imediatamente à esquerda da casa branca
+    bebida[1], cor_casa[1],  # 5 - O dono da casa verde bebe café
+    cigarro[1], profissao[2],  # 6 - O que fuma Pall Mall é professor
+    cor_casa[2], cigarro[2],  # 7 - O dono da casa amarela fuma Dunhill
+    cor_casa[2],  # 8 - O que mora na casa do meio bebe leite
+    nacionalidade[3],  # 9 - O norueguês mora na primeira casa
+    cigarro[3], profissao[3],  # 10 - O que fuma Blends mora ao lado do empresário
+    profissao[4], cigarro[2],  # 11 - O programador mora ao lado do que fuma Dunhill
+    cigarro[4], bebida[4],  # 12 - O que fuma Bluemasters bebe cerveja
+    nacionalidade[4], cigarro[3],  # 13 - O alemão fuma Prince
+    nacionalidade[3], cor_casa[3],  # 14 - O norueguês mora ao lado da casa azul
+    cigarro[3], bebida[0]  # 15 - O que fuma Blends mora ao lado do que bebe água
+)
+
+# Definição da consulta para descobrir quem é o terrorista
+query = And(
+    terrorista[0], terrorista[1], terrorista[2], terrorista[3], terrorista[4],
+    Not(knowledge_base)  # O terrorista é alguém que não satisfaz as condições conhecidas
+)
+
+# Função para verificar todas as combinações possíveis para descobrir o terrorista
+def find_terrorist():
+    symbols = nacionalidade + cor_casa + bebida + cigarro + profissao + terrorista
+    permutations_list = permutations([True, False] * len(symbols), len(symbols))
+    for perm in permutations_list:
+        model = {symbol: value for symbol, value in zip(symbols, perm)}
+        if query.evaluate(model):
+            return model
+    return None
+
+# Encontrar o terrorista
+terrorist_model = find_terrorist()
+
+# Mostrar resultado
+if terrorist_model:
+    print("Terrorista encontrado:")
+    for symbol, value in terrorist_model.items():
+        if symbol in terrorista and value:
+            print(f"{symbol} é o terrorista.")
+else:
+    print("Não foi possível determinar o terrorista com as informações fornecidas.")
--- gps.py
+++ gps.py
@@ -29,6 +29,8 @@ def bfs_encontrar_caminho(matriz, origem, destino):
                 novo_caminho = list(caminho)
                 novo_caminho.append(vizinho)
                 fila.append(novo_caminho)
+                # Impressão das coordenadas percorridas
+                print(f"Coordenada percorrida no BFS: {vizinho}")
 
     return None
 
@@ -60,6 +62,8 @@ def dfs_encontrar_caminho(matriz, origem, destino):
                 if vizinho not in visitados:
                     pilha.append(vizinho)
                     caminho[vizinho] = atual
+                    # Impressão das coordenadas percorridas
+                    print(f"Coordenada percorrida no DFS: {vizinho}")
 
     return None
 
@@ -79,10 +83,8 @@ def imprimir_matriz(matriz):
         print(" ".join(str(elem) for elem in linha))
 
 # Função para imprimir o caminho encontrado na matriz
-# Função para imprimir o caminho encontrado na matriz
 def imprimir_caminho_encontrado(matriz, caminho):
     if caminho is None:
-        print("Nenhum caminho encontrado.")
         return
     
     caminho_set = set(caminho)
@@ -94,11 +96,42 @@ def imprimir_caminho_encontrado(matriz, caminho):
                 print(elem, end=" ")
         print()
 
-
 # Gerar uma matriz de exemplo (labirinto)
-def gerar_labirinto(rows, cols):
-    labirinto = [[random.randint(0, 1) for _ in range(cols)] for _ in range(rows)]
-    return labirinto
+def gerar_cidade(linhas, colunas):
+    cidade = [[0 for _ in range(colunas)] for _ in range(linhas)]
+
+    # Adiciona ruas horizontais
+    for i in range(1, linhas, 2):
+        cidade[i] = [1] * colunas
+
+    # Adiciona ruas verticais
+    for j in range(1, colunas, 2):
+        for i in range(linhas):
+            cidade[i][j] = 1
+
+    # Adiciona interseções aleatórias
+    for i in range(1, linhas, 2):
+        for j in range(1, colunas, 2):
+            cidade[i][j] = 1
+
+    return cidade
+
+# Exemplo de uso
+linhas = 100
+colunas = 100
+cidade = gerar_cidade(linhas, colunas)
+
+
+def coloca_impedimentos(labirinto):
+   labirinto[3][5] = 2
+   labirinto[3][1] = 2
+   labirinto[3][2] = 2
+   labirinto[3][3] = 2
+   labirinto[3][0] = 2
+   labirinto[3][4] = 2
+   labirinto[3][6] = 2
+   labirinto[3][7] = 2
+   labirinto[3][8] = 2
 
 # Definir pontos de origem e destino aleatórios na matriz
 def pontos_aleatorios(matriz):
@@ -112,8 +145,8 @@ def pontos_aleatorios(matriz):
     return origem, destino
 
 # Testar e comparar os algoritmos com a matriz gerada
-labirinto = gerar_labirinto(10, 10)  # Tamanho da matriz (10x10) - pode ser ajustado
-
+labirinto = gerar_cidade(50,50)  # Tamanho da matriz (10x10) - pode ser ajustado
+coloca_impedimentos(labirinto)
 origem, destino = pontos_aleatorios(labirinto)
 
 print("Matriz gerada:")
@@ -122,20 +155,14 @@ print()
 
 print(f"Origem: {origem}, Destino: {destino}")
 
-# Avaliar tempo de execução do BFS - Busca em Largura
-tempo_bfs = timeit.timeit(lambda: bfs_encontrar_caminho(labirinto, origem, destino), number=1)
-print(f"Tempo de execução do BFS: {tempo_bfs:.6f} segundos")
-
-# Avaliar tempo de execução do DFS - Busca em Profundidade
-tempo_dfs = timeit.timeit(lambda: dfs_encontrar_caminho(labirinto, origem, destino), number=1)
-print(f"Tempo de execução do DFS: {tempo_dfs:.6f} segundos")
-
-# Encontrar o caminho usando BFS e imprimir na matriz
+# Encontrar o caminho usando BFS e imprimir na mclearatriz
+print("\nExecutando BFS...")
 caminho_encontrado_bfs = bfs_encontrar_caminho(labirinto, origem, destino)
 print("\nCaminho encontrado pelo BFS:")
 imprimir_caminho_encontrado(labirinto, caminho_encontrado_bfs)
 
 # Encontrar o caminho usando DFS e imprimir na matriz
+print("\nExecutando DFS...")
 caminho_encontrado_dfs = dfs_encontrar_caminho(labirinto, origem, destino)
 print("\nCaminho encontrado pelo DFS:")
 imprimir_caminho_encontrado(labirinto, caminho_encontrado_dfs)
