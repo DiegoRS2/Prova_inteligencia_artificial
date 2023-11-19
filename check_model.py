b2941fbd66d27eda517957bc335a1c9710c17bd3
Gian <giglio2001@gmail.com>
2023-11-19 19:21:06 -0300

    commit

--- check_model.py
+++ check_model.py
@@ -1,51 +1,135 @@
 from itertools import permutations
 
+class Sentence():
 
-class Symbol():
+    def evaluate(self, model):
+        """Evaluates the logical sentence."""
+        raise Exception("nothing to evaluate")
+
+    def formula(self):
+        """Returns string formula representing logical sentence."""
+        return ""
+
+    def symbols(self):
+        """Returns a set of all symbols in the logical sentence."""
+        return set()
+
+    @classmethod
+    def validate(cls, sentence):
+        if not isinstance(sentence, Sentence):
+            raise TypeError("must be a logical sentence")
+
+    @classmethod
+    def parenthesize(cls, s):
+        """Parenthesizes an expression if not already parenthesized."""
+        def balanced(s):
+            """Checks if a string has balanced parentheses."""
+            count = 0
+            for c in s:
+                if c == "(":
+                    count += 1
+                elif c == ")":
+                    if count <= 0:
+                        return False
+                    count -= 1
+            return count == 0
+        if not len(s) or s.isalpha() or (
+            s[0] == "(" and s[-1] == ")" and balanced(s[1:-1])
+        ):
+            return s
+        else:
+            return f"({s})"
+
+class Symbol(Sentence):
+
     def __init__(self, name):
         self.name = name
-
-    def __repr__(self):
-        return self.name
 
     def __eq__(self, other):
         return isinstance(other, Symbol) and self.name == other.name
 
     def __hash__(self):
-        return hash(self.name)
-
-
-class Symbol:
-    def __init__(self, name):
-        self.name = name
+        return hash(("symbol", self.name))
 
     def __repr__(self):
         return self.name
 
     def evaluate(self, model):
-        return model[self]
+        try:
+            return bool(model[self.name])
+        except KeyError:
+            print(f"variable {self.name} not in model")
 
-    def __eq__(self, other):
-        return isinstance(other, Symbol) and self.name == other.name
+    def formula(self):
+        return self.name
 
-    def __hash__(self):
-        return hash(self.name)
+    def symbols(self):
+        return {self.name}
 
 
-class And:
-    def __init__(self, *conjuncts):
+class And(Sentence):
+    def __init__(self, *conjuncts: 'Sentence'):
+        for conjunct in conjuncts:
+            Sentence.validate(conjunct)
         self.conjuncts = list(conjuncts)
 
     def __repr__(self):
         conjunctions = ", ".join([str(conjunct) for conjunct in self.conjuncts])
         return f"And({conjunctions})"
 
+    # def evaluate(self, model):
+    #     return all(conjunct.evaluate(model) if isinstance(conjunct, Symbol) else conjunct for conjunct in self.conjuncts)
+    
     def evaluate(self, model):
-        return all(conjunct.evaluate(model) if isinstance(conjunct, Symbol) else conjunct for conjunct in self.conjuncts)
+        return all(conjunct.evaluate(model) for conjunct in self.conjuncts)
+
+    def formula(self):
+        if len(self.conjuncts) == 1:
+            return self.conjuncts[0].formula()
+        return " ∧ ".join([Sentence.parenthesize(conjunct.formula())
+                           for conjunct in self.conjuncts])
+
+    def symbols(self):
+        return set.union(*[conjunct.symbols() for conjunct in self.conjuncts])
+    
+    def add(self, conjunct):
+        Sentence.validate(conjunct)
+        self.conjuncts.append(conjunct)
+    
 
 
-class Not:
-    def __init__(self, operand):
+class Or(Sentence):
+    def __init__(self, *disjuncts: 'Sentence'):
+        for disjunct in disjuncts:
+            Sentence.validate(disjunct)
+        self.disjuncts = list(disjuncts)
+
+    def __eq__(self, other):
+        return isinstance(other, Or) and self.disjuncts == other.disjuncts
+
+    def __hash__(self):
+        return hash(
+            ("or", tuple(hash(disjunct) for disjunct in self.disjuncts))
+        )
+
+    def __repr__(self):
+        disjuncts = ", ".join([str(disjunct) for disjunct in self.disjuncts])
+        return f"Or({disjuncts})"
+
+    def evaluate(self, model):
+        return any(disjunct.evaluate(model) for disjunct in self.disjuncts)
+
+    def formula(self):
+        if len(self.disjuncts) == 1:
+            return self.disjuncts[0].formula()
+        return " ∨  ".join([Sentence.parenthesize(disjunct.formula())
+                            for disjunct in self.disjuncts])
+
+    def symbols(self):
+        return set.union(*[disjunct.symbols() for disjunct in self.disjuncts])
+
+class Not(Sentence):
+    def __init__(self, operand: 'Sentence'):
         self.operand = operand
 
     def __repr__(self):
@@ -63,47 +147,150 @@ def generate_symbols(prefix, count):
 
 
 # Definição dos símbolos para cada propriedade
-nacionalidade = generate_symbols('n', 5)
-cor_casa = generate_symbols('c', 5)
-bebida = generate_symbols('b', 5)
-cigarro = generate_symbols('s', 5)
-profissao = generate_symbols('p', 5)
-terrorista = generate_symbols('t', 5)
+pos_casas = generate_symbols('pc', 5)
+nacionalidades = generate_symbols('n', 5)
+cor_casas = generate_symbols('c', 5)
+bebidas = generate_symbols('b', 5)
+cigarros = generate_symbols('s', 5)
+profissoes = generate_symbols('p', 5)
+
+knowledge_base = And()
+
+for nacionalidade in nacionalidades:
+    for nacionalidade_temp in nacionalidades:
+        if (nacionalidade != nacionalidade_temp):
+            knowledge_base.add(And(nacionalidade, Or(pos_casas[0],pos_casas[1],pos_casas[2],pos_casas[3],pos_casas[4]),Not(And(nacionalidade_temp))))
+
+    for nacionalidade_temp in nacionalidades:
+        if (nacionalidade != nacionalidade_temp):
+            knowledge_base.add(And(nacionalidade, Or(cor_casas[0],cor_casas[1],cor_casas[2],cor_casas[3],cor_casas[4]),Not(And(nacionalidade_temp))))
+
+    for nacionalidade_temp in nacionalidades:
+        if (nacionalidade != nacionalidade_temp):
+            knowledge_base.add(And(nacionalidade, Or(bebidas[0],bebidas[1],bebidas[2],bebidas[3],bebidas[4]),Not(And(nacionalidade_temp))))
+
+    for nacionalidade_temp in nacionalidades:
+        if (nacionalidade != nacionalidade_temp):
+            knowledge_base.add(And(nacionalidade, Or(cigarros[0],cigarros[1],cigarros[2],cigarros[3],cigarros[4]),Not(And(nacionalidade_temp))))
+
+    for nacionalidade_temp in nacionalidades:
+        if (nacionalidade != nacionalidade_temp):
+            knowledge_base.add(And(nacionalidade, Or(profissoes[0],profissoes[1],profissoes[2],profissoes[3],profissoes[4]),Not(And(nacionalidade_temp))))
+
+
+def model_check(knowledge, query):
+    """Checks if knowledge base entails query."""
+
+    def check_all(knowledge, query, symbols, model):
+        """Checks if knowledge base entails query, given a particular model."""
+
+        # If model has an assignment for each symbol
+        if not symbols:
+
+            # If knowledge base is true in model, then query must also be true
+            if knowledge.evaluate(model):
+                return query.evaluate(model)
+            return True
+        else:
+
+            # Choose one of the remaining unused symbols
+            remaining = symbols.copy()
+            p = remaining.pop()
+
+            # Create a model where the symbol is true
+            model_true = model.copy()
+            model_true[p] = True
+
+            # Create a model where the symbol is false
+            model_false = model.copy()
+            model_false[p] = False
+
+            # Ensure entailment holds in both models
+            return (check_all(knowledge, query, remaining, model_true) and
+                    check_all(knowledge, query, remaining, model_false))
+
+    # Get all symbols in both knowledge and query
+    symbols = set.union(knowledge.symbols(), query.symbols())
+
+    # Check that knowledge entails query
+    return check_all(knowledge, query, symbols, dict())
+
+# for nacionalidade in nacionalidades:
+#     knowledge_base = And(nacionalidade, Or())
+
+#pos_casa1 = 1
+#pos_casa2 = 2
+#pos_casa3 = 3
+#pos_casa4 = 4
+#pos_casa5 = 5
+
+#britanico = 1
+#suico = 2
+#dinamarquês = 3
+#norueguês = 4
+#alemão = 5
+
+#casa vermelha = 1
+#casa verde = 2
+#casa branca = 3
+#casa amarela = 4
+#casa azul = 5
+
+#empresario = 1
+#mecanico = 2
+#professor = 3
+#programador = 4
+#medico = 5
+
+#leite = 1
+#cafe = 2
+#cha = 3
+#cerveja = 4
+#agua = 5
+
+#Blends = 1
+#Pall Mall = 2
+#Dunhill = 3
+#Prince = 4
+#Bluemasters = 5
 
 # Definição das sentenças lógicas representando as informações adicionais
-knowledge_base = And(
-    nacionalidade[0], cor_casa[0],  # 1 - O britânico mora na casa vermelha
-    nacionalidade[1], profissao[1],  # 2 - O suíço é mecânico
-    nacionalidade[2], bebida[2],  # 3 - O dinamarquês bebe chá
-    cor_casa[1], cor_casa[0],  # 4 - A casa verde fica imediatamente à esquerda da casa branca
-    bebida[1], cor_casa[1],  # 5 - O dono da casa verde bebe café
-    cigarro[1], profissao[2],  # 6 - O que fuma Pall Mall é professor
-    cor_casa[2], cigarro[2],  # 7 - O dono da casa amarela fuma Dunhill
-    cor_casa[2],  # 8 - O que mora na casa do meio bebe leite
-    nacionalidade[3],  # 9 - O norueguês mora na primeira casa
-    cigarro[3], profissao[3],  # 10 - O que fuma Blends mora ao lado do empresário
-    profissao[4], cigarro[2],  # 11 - O programador mora ao lado do que fuma Dunhill
-    cigarro[4], bebida[4],  # 12 - O que fuma Bluemasters bebe cerveja
-    nacionalidade[4], cigarro[3],  # 13 - O alemão fuma Prince
-    nacionalidade[3], cor_casa[3],  # 14 - O norueguês mora ao lado da casa azul
-    cigarro[3], bebida[0]  # 15 - O que fuma Blends mora ao lado do que bebe água
-)
+knowledge_base.conjuncts.append(And(
+    And(nacionalidades[0],cor_casas[0]),
+    And(nacionalidades[1],profissoes[1]),
+    And(nacionalidades[2],bebidas[2]),
+    Not(And(cor_casas[1], pos_casas[4])),
+    And(cor_casas[1],bebidas[1]),
+    And(cigarros[1],profissoes[2]),
+    And(cor_casas[3],cigarros[2]),
+    And(bebidas[0], pos_casas[2]),
+    And(nacionalidades[3], pos_casas[0]),
+    And(cigarros[4],bebidas[3]),
+    And(nacionalidades[4],cigarros[3]),
+))
 
 # Definição da consulta para descobrir quem é o terrorista
-query = And(
-    terrorista[0], terrorista[1], terrorista[2], terrorista[3], terrorista[4],
-    Not(knowledge_base)  # O terrorista é alguém que não satisfaz as condições conhecidas
-)
+# query = And(
+#     #terrorista[0], terrorista[1], terrorista[2], terrorista[3], terrorista[4],
+#     Not()  # O terrorista é alguém que não satisfaz as condições conhecidas
+# )
 
 # Função para verificar todas as combinações possíveis para descobrir o terrorista
 def find_terrorist():
-    symbols = nacionalidade + cor_casa + bebida + cigarro + profissao + terrorista
-    permutations_list = permutations([True, False] * len(symbols), len(symbols))
-    for perm in permutations_list:
-        model = {symbol: value for symbol, value in zip(symbols, perm)}
-        if query.evaluate(model):
-            return model
-    return None
+    symbols = nacionalidades + cor_casas + bebidas + cigarros + profissoes
+
+    simbolos = []
+    # permutations_list = permutations([True, False] * len(symbols), len(symbols))
+    # for perm in permutations_list:
+    #     model = {symbol: value for symbol, value in zip(symbols, perm)}
+    #     if knowledge_base.evaluate(model):
+    #         return model
+    for symbol in symbols:
+        if model_check(knowledge_base, symbol):
+            simbolos.append(symbol)
+    return simbolos
+
+
 
 # Encontrar o terrorista
 terrorist_model = find_terrorist()
@@ -112,7 +299,6 @@ terrorist_model = find_terrorist()
 if terrorist_model:
     print("Terrorista encontrado:")
     for symbol, value in terrorist_model.items():
-        if symbol in terrorista and value:
-            print(f"{symbol} é o terrorista.")
+        print(f"{symbol} é o terrorista.")
 else:
     print("Não foi possível determinar o terrorista com as informações fornecidas.")
